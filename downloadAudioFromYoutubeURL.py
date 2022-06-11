475cb354e1b2b86b4f152e087708026b09b0d71d
new codi <newcodingage@gmail.com>
2022-06-11 09:59:59 +0800

    UPDATE: fixed UI using pyqtsignal & thread

--- downloadAudioFromYoutubeURL.py
+++ downloadAudioFromYoutubeURL.py
@@ -1,3 +1,4 @@
+from concurrent.futures import thread
 import ffmpeg
 import os
 from enum import Enum
@@ -7,11 +8,26 @@ from youtube_dl import YoutubeDL as ytdl
 import PyQt5.QtWidgets as qtw 
 import PyQt5.QtGui as qtg
 
+import threading
+import time
+from PyQt5.QtCore import QObject, QThread, pyqtSignal
+
+class Worker(QObject):
+	message = pyqtSignal(str)
+	progress = pyqtSignal(int)
+
+	def updateProgress(self, p):
+		self.progress.emit(p)
+	
+	def updateMessage(self, msg):
+		self.message.emit(msg)
+	
+
 class DownloadOption(Enum):
 	OK		= 1
 	Error	= 0
 
-class MainWindow(qtw.QWidget):
+class MainWindow(qtw.QWidget, QObject):
 	def __init__(self):
 		super().__init__()
 		self.fileName = ''
@@ -25,7 +41,7 @@ class MainWindow(qtw.QWidget):
 		# create a label
 		self.textLabel = qtw.QLabel("Enter the URL/Link")
 		# change font size of label
-		self.textLabel.setFont(qtg.QFont('Helevtica', 18))
+		# self.textLabel.setFont(qtg.QFont('Helevtica', 18))
 		self.layout().addWidget(self.textLabel)
 
 		hbox = qtw.QHBoxLayout()
@@ -34,7 +50,7 @@ class MainWindow(qtw.QWidget):
 		self.audioCheckb = qtw.QCheckBox("Audio")
 		self.audioCheckb.setChecked(True)
 		self.needConvertCheckb = qtw.QCheckBox("Convert")
-		self.needConvertCheckb.setChecked(False)
+		self.needConvertCheckb.setChecked(True)
 		self.extensionText = qtw.QLineEdit()
 		self.extensionText.setObjectName("extension")
 		self.extensionText.setText(".mp3")
@@ -48,27 +64,49 @@ class MainWindow(qtw.QWidget):
 		# create progress bar
 		self.progressBar = qtw.QProgressBar()
 		self.progressBar.setFixedWidth(270)
-		# self.progressBar.setFixedHeight(5)
 		self.layout().addWidget(self.progressBar)
 
 		# create a textfield
 		self.textField = qtw.QLineEdit()
 		self.textField.setObjectName("ytLink")
-		self.textField.setText("")
+		self.textField.setText("https://youtu.be/NDFeKOLDcfc") # dummy video
 		self.layout().addWidget(self.textField)
 
 		# create a button
 		self.button = qtw.QPushButton("Download", clicked = lambda: self.startDownload())
 		self.layout().addWidget(self.button)
 
+		self.UIWorker = Worker()
+		self.setupWorker()
+		self.tempCnt = 1
+
 		self.show()
+	
+	def updateProgress(self, msg):
+		self.textLabel.setText(msg)
+	
+	def updateTextWithProgress(self, percentage):
+		self.progressBar.setValue(percentage)
+		if self.tempCnt % 1:
+			self.textLabel.setText(f"({percentage:02}%) Downloading .")
+		elif self.tempCnt % 2:
+			self.textLabel.setText(f"({percentage:02}%) Downloading ..")
+		elif self.tempCnt % 3:
+			self.textLabel.setText(f"({percentage:02}%) Downloading ...")
+		else:
+			self.tempCnt = 0
+			self.textLabel.setText(f"({percentage:02}%) Downloading .")
+		self.tempCnt += 1
+
+	def setupWorker(self):
+		self.UIWorker.message.connect(self.updateProgress)
+		self.UIWorker.progress.connect(self.updateTextWithProgress)
 
 	def startDownload(self):
 		youtube_url = self.textField.text()
-		self.findDownloadType()
-		# print(f"check box: video {self.videoCheckB.isChecked()} 
-		# audio {self.audioCheckb.isChecked()}")
-		self.donwloader(youtube_url)
+
+		self.downloadTask = threading.Thread(target=self.donwloader, args=(youtube_url,))
+		self.downloadTask.start()
 
 	def findDownloadType(self):
 		vcb = self.videoCheckB.isChecked()
@@ -83,45 +121,45 @@ class MainWindow(qtw.QWidget):
 			dlType = 'bestvideo/best'
 		elif acb:
 			dlType = 'bestaudio/best[ext='+ext+']'
-		else:
-			return DownloadOption.Error
 
 		ytOption['format'] = dlType
 		ytOption['progress_hooks'] = [self.my_hook]
 
-		return DownloadOption.OK, ytOption
+		return ytOption
 
 	def my_hook(self, d):
 		if d['status'] == 'finished':
-			self.textLabel.setText("Done!")
+			self.UIWorker.updateMessage("Done!")
 			self.fileName = d['filename']
 
 		if d['status'] == 'downloading':
-			progressPercentage = d['_percent_str']
-			progressPercentage = progressPercentage.replace('%','')
-			progressPercentage = float(progressPercentage)
-			# print(d['filename'], d['_percent_str'], d['_eta_str'])
-			self.progressBar.setValue(int(progressPercentage))
+			percen = d['_percent_str'].replace('%','')
+			percen_int = int(float(percen))
+			self.UIWorker.updateProgress(percen_int)
 
 	def donwloader(self, URL):
+		self.button.setEnabled(False)
+		msg = "Prepare download"
 		if "https" in URL:
-			self.textLabel.setText("Downloading...")
-			dlOption, setting = self.findDownloadType()
-			if dlOption is not DownloadOption.Error:
-				try:
-					audio_downloader = ytdl(setting)
-					audio_downloader.extract_info(URL)
-				except :
-					self.textLabel.setText("Download error!")
-				finally:
-					if self.needConvertCheckb.isChecked():
-						if self.extensionText.text() not in self.fileName:
-							self.convertFileTo(self.fileName)
+			try:
+				self.UIWorker.updateMessage(msg)
+				audio_downloader = ytdl(self.findDownloadType())
+				audio_downloader.extract_info(URL)
+			except :
+				msg = "Download error!"
+				self.UIWorker.updateMessage(msg)
+			finally:
+				if self.needConvertCheckb.isChecked():
+					if self.extensionText.text() not in self.fileName:
+						self.convertFileTo(self.fileName)
+				else:
+					self.button.setEnabled(True)
 		else:
-			self.textLabel.setText("invalid URL or links")
+			msg = "Incorrect Links!"
+			self.UIWorker.updateMessage(msg)
 
 	def convertFileTo(self, file):
-		self.textLabel.setText("Converting file..")
+		self.UIWorker.updateMessage("Converting files")
 		outputFile = os.path.splitext(file)[0]+self.extensionText.text()
 		
 		stream = ffmpeg.input(file)
@@ -130,8 +168,12 @@ class MainWindow(qtw.QWidget):
 			ffmpeg.run(stream)
 			#remove original file
 			os.remove(file)
+
+			self.UIWorker.updateMessage("Finished!")
 		except:
-			self.textLabel.setText("Conversion Fail!")
+			self.UIWorker.updateMessage("Convert Fail!")
+		
+		self.button.setEnabled(True)
 
 def main():
 	app = qtw.QApplication([])
